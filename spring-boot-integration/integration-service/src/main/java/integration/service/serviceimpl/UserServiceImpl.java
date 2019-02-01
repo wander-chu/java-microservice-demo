704699f06afed38ebf747e2c0e1704ec9673cde5
wander-chu <362207958@qq.com>
2019-02-01 12:30:21 +0800

    add

---
+++ spring-boot-integration/integration-service/src/main/java/integration/service/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package integration.service.serviceimpl;
+
+import integration.base.domain.User;
+import integration.base.service.UserService;
+import integration.service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findUserById(long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void edit(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void delete(long id) {
+        userRepository.deleteById(id);
+    }
+}
---
+++ spring-boot-integration/integration-web/src/main/java/integration/IntegrationWebApplication.java
@@ -0,0 +1,14 @@
+package integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationWebApplication.class, args);
+    }
+
+}
+
